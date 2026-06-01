9b96701ac1d219b3a6e98113b6d0a0c5512f9e1a
Student <student@university.edu>
2026-06-01 15:57:46 +0800

    Optimize TURN configuration with TCP and UDP endpoints to maximize compatibility

--- app.py
+++ app.py
@@ -467,7 +467,22 @@ else:
                             "iceServers": [
                                 {"urls": ["stun:stun.l.google.com:19302"]},
                                 {
-                                    "urls": ["turn:openrelay.metered.ca:80", "turn:openrelay.metered.ca:443"],
+                                    "urls": ["turn:openrelay.metered.ca:80?transport=udp"],
+                                    "username": "openrelayproject",
+                                    "credential": "openrelayproject"
+                                },
+                                {
+                                    "urls": ["turn:openrelay.metered.ca:80?transport=tcp"],
+                                    "username": "openrelayproject",
+                                    "credential": "openrelayproject"
+                                },
+                                {
+                                    "urls": ["turn:openrelay.metered.ca:443?transport=udp"],
+                                    "username": "openrelayproject",
+                                    "credential": "openrelayproject"
+                                },
+                                {
+                                    "urls": ["turn:openrelay.metered.ca:443?transport=tcp"],
                                     "username": "openrelayproject",
                                     "credential": "openrelayproject"
                                 }
@@ -626,7 +641,22 @@ else:
                         "iceServers": [
                             {"urls": ["stun:stun.l.google.com:19302"]},
                             {
-                                "urls": ["turn:openrelay.metered.ca:80", "turn:openrelay.metered.ca:443"],
+                                "urls": ["turn:openrelay.metered.ca:80?transport=udp"],
+                                "username": "openrelayproject",
+                                "credential": "openrelayproject"
+                            },
+                            {
+                                "urls": ["turn:openrelay.metered.ca:80?transport=tcp"],
+                                "username": "openrelayproject",
+                                "credential": "openrelayproject"
+                                },
+                            {
+                                "urls": ["turn:openrelay.metered.ca:443?transport=udp"],
+                                "username": "openrelayproject",
+                                "credential": "openrelayproject"
+                            },
+                            {
+                                "urls": ["turn:openrelay.metered.ca:443?transport=tcp"],
                                 "username": "openrelayproject",
                                 "credential": "openrelayproject"
                             }
